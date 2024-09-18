29d84dcec7baba62374379326feb338ce85f9353
Dilkash Peshimam <dilkashpeshimam@gmail.com>
2024-09-18 12:15:56 +0530

    change token amount

--- contract/contracts/VotingToken.sol
+++ contract/contracts/VotingToken.sol
@@ -5,6 +5,6 @@ import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 
 contract DvoteCoin is ERC20, ERC20Permit {
     constructor() ERC20("DvoteCoin", "DV") ERC20Permit("DvoteCoin") {
-        _mint(msg.sender, 10000 * 10**decimals());
+        _mint(msg.sender, 100000 * 10**decimals());
     }
 }
